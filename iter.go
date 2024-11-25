654f7d5b3b6e6977e7397d8968cdff03ee10a278
Yannik <yannik@printingambitions.com>
2024-11-25 11:50:15 +0100

    dev

--- iter.go
+++ iter.go
@@ -2,31 +2,31 @@ package gostd
 
 import "iter"
 
-func IntoIter[T any](s []T) iter.Seq[T] {
-	return func(yield func(T) bool) {
-		for _, a := range s {
-			if !yield(a) {
+func IntoIter[T any](s *[]T) iter.Seq[*T] {
+	return func(yield func(*T) bool) {
+		for i := range *s {
+			if !yield(&(*s)[i]) {
 				return
 			}
 		}
 	}
 }
 
-func Map[T, U any](seq iter.Seq[T], f func(T) U) iter.Seq[U] {
+func Map[T, U any](seq *iter.Seq[T], f func(*T) U) iter.Seq[U] {
 	return func(yield func(U) bool) {
-		for a := range seq {
-			if !yield(f(a)) {
+		for a := range *seq {
+			if !yield(f(&a)) {
 				return
 			}
 		}
 	}
 }
 
-func Filter[T any](seq iter.Seq[T], f func(T) bool) iter.Seq[T] {
-	return func(yield func(T) bool) {
-		for a := range seq {
-			if f(a) {
-				if !yield(a) {
+func Filter[T any](seq *iter.Seq[T], f func(*T) bool) iter.Seq[*T] {
+	return func(yield func(*T) bool) {
+		for a := range *seq {
+			if f(&a) {
+				if !yield(&a) {
 					return
 				}
 			}
@@ -34,43 +34,43 @@ func Filter[T any](seq iter.Seq[T], f func(T) bool) iter.Seq[T] {
 	}
 }
 
-func ForEach[T any](seq iter.Seq[T], f func(T)) {
-	for a := range seq {
-		f(a)
+func ForEach[T any](seq *iter.Seq[T], f func(*T)) {
+	for a := range *seq {
+		f(&a)
 	}
 }
 
-func Every[T any](seq iter.Seq[T], f func(T) bool) bool {
-	for a := range seq {
-		if !f(a) {
+func Every[T any](seq *iter.Seq[T], f func(*T) bool) bool {
+	for a := range *seq {
+		if !f(&a) {
 			return false
 		}
 	}
 	return true
 }
 
-func Some[T any](seq iter.Seq[T], f func(T) bool) bool {
-	for a := range seq {
-		if f(a) {
+func Some[T any](seq *iter.Seq[T], f func(*T) bool) bool {
+	for a := range *seq {
+		if f(&a) {
 			return true
 		}
 	}
 	return false
 }
 
-func Find[T any](seq iter.Seq[T], f func(T) bool) (*T, bool) {
-	for a := range seq {
-		if f(a) {
+func Find[T any](seq *iter.Seq[T], f func(*T) bool) (*T, bool) {
+	for a := range *seq {
+		if f(&a) {
 			return &a, true
 		}
 	}
 	return nil, false
 }
 
-func IndexOf[T any](seq iter.Seq[T], f func(T) bool) int {
+func IndexOf[T any](seq *iter.Seq[T], f func(*T) bool) int {
 	i := 0
-	for a := range seq {
-		if f(a) {
+	for a := range *seq {
+		if f(&a) {
 			return i
 		}
 		i++
